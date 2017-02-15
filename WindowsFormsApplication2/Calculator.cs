cd8de1ca4852965da0d202dfdf185f556f84b71a
GENYÓ <Dell 7010DT@DELL7010DT-PC>
2017-02-15 21:33:52 +0100

    Calculator

---
+++ WindowsFormsApplication2/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public double Összeadás(double x, double y)
+        {
+
+            return x + y;
+        }
+
+        public double Kivonás(double x, double y)
+        {
+
+            return x - y;
+        }
+
+        public double Szorzás(double x, double y)
+        {
+
+            return x * y;
+        }
+
+        public double Osztás(double x, double y)
+        {
+
+            return x / y;
+        }
+    }
+}
