0e95386f849bdb3ad6d4ec4c76d0b375aaf719db
dragonar <fugedi4@gmail.com>
2017-02-15 23:01:32 +0100

    kész

--- WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -56,7 +56,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -66,7 +66,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button_Click);
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -76,7 +76,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button_Click);
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -86,7 +86,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button_Click);
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -96,7 +96,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button_Click);
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -106,7 +106,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button_Click);
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -116,7 +116,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button_Click);
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -126,7 +126,7 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button_Click);
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -136,7 +136,7 @@
             this.button9.TabIndex = 8;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button_Click);
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -146,7 +146,7 @@
             this.button10.TabIndex = 9;
             this.button10.Text = "0";
             this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button_Click);
+            this.button10.Click += new System.EventHandler(this.button0_Click);
             // 
             // button11
             // 
@@ -156,6 +156,7 @@
             this.button11.TabIndex = 10;
             this.button11.Text = "/";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.osztas_Click);
             // 
             // button12
             // 
@@ -165,6 +166,7 @@
             this.button12.TabIndex = 11;
             this.button12.Text = "*";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.szorzas_Click);
             // 
             // button13
             // 
@@ -174,6 +176,7 @@
             this.button13.TabIndex = 12;
             this.button13.Text = "-";
             this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.kivonas_Click);
             // 
             // button14
             // 
@@ -183,6 +186,7 @@
             this.button14.TabIndex = 13;
             this.button14.Text = "+";
             this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.oszead_Click);
             // 
             // button15
             // 
@@ -192,6 +196,7 @@
             this.button15.TabIndex = 14;
             this.button15.Text = "=";
             this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.enter_Click);
             // 
             // button16
             // 
--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -19,8 +19,9 @@ namespace WindowsFormsApplication2
 
         double x;
         double y;
+        char operation;
 
-        
+
         private void C_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
@@ -30,16 +31,124 @@ namespace WindowsFormsApplication2
 
         private void CE_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = "0";
         }
 
-        private void button_Click(object sender, EventArgs e)
+        private void button0_Click(object sender, EventArgs e)
         {
-            Button b = (Button)sender;
-            textBox1.Text = textBox1.Text +b;
+           
+            if (textBox1.Text.Contains("nulával nem osztunk"))
+            {
+                textBox1.Clear();
+            }
 
+           
+            else if (!textBox1.Text.StartsWith("0") || textBox1.Text.Contains("."))
+            {
+                
+                textBox1.Text += "0";
+            }
         }
 
-       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text.Contains("nulával nem osztunk"))
+            {
+                textBox1.Clear();
+            }
+          
+            textBox1.Text += "1";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text.Contains("nulával nem osztunk"))
+            {
+                textBox1.Clear();
+            }
+            
+            textBox1.Text += "4";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text.Contains("nulával nem osztunk"))
+            {
+                textBox1.Clear();
+            }
+           
+            textBox1.Text += "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+            if (textBox1.Text.Contains("nulával nem osztunk"))
+            {
+                textBox1.Clear();
+            }
+           
+            textBox1.Text += "2";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text.Contains("nulával nem osztunk"))
+            {
+                textBox1.Clear();
+            }
+         
+            textBox1.Text += "5";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+          
+            if (textBox1.Text.Contains("nulával nem osztunk"))
+            {
+                textBox1.Clear();
+            }
+           
+            textBox1.Text += "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text.Contains("nulával nem osztunk"))
+            {
+                textBox1.Clear();
+            }
+          
+            textBox1.Text += "3";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text.Contains("nulával nem osztunk"))
+            {
+                textBox1.Clear();
+            }
+            
+            textBox1.Text += "6";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text.Contains("nulával nem osztunk"))
+            {
+                textBox1.Clear();
+            }
+           
+            textBox1.Text += "9";
+        }
+
+
     }
 }
