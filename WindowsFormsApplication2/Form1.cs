0bd743492023a69e008d7c4c56901e299bbea357
GENYÓ <Dell 7010DT@DELL7010DT-PC>
2017-02-15 21:52:56 +0100

    lll

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -16,5 +16,54 @@ namespace WindowsFormsApplication2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            private void btnEquals_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "&quot
+            {
+
+                Calculator myCalculator = new Calculator();
+
+                if (double.TryParse(txtDisplay.Text, out y))
+
+                    switch (operation)
+                    {
+                        case '+':
+                           
+                            textBox1.Text = (myCalculator.Add(x, y)).ToString();
+                            break;
+
+                        case '-':
+                            textBox1.Text = (myCalculator.Subtract(x, y)).ToString();
+                            break;
+                    }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out x))
+            {
+                operation = '+';
+                textBox1.Clear();
+            }
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out x))
+            {
+                operation = '-';
+                textBox1.Clear();
+            }
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out x))
+            {
+                operation = '*';
+            }
+
+        }
     }
 }
